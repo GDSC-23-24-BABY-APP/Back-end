db2726c39292507777740271a970ff4bea6db2a5
Leeyoujin00 <cymn6032@gmail.com>
2024-01-04 14:33:28 +0900

    Create initialize setting

---
+++ src/main/java/com/app/premom/PremomApplication.java
@@ -0,0 +1,13 @@
+package com.app.premom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/premom/PremomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.premom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
