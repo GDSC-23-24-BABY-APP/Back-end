1c0171c1c37c45b66b2c0389907a7eab8266ef04
Leeyoujin00 <cymn6032@gmail.com>
2024-02-08 19:22:55 +0900

    chore: 설정 수정

--- src/test/java/com/app/premom/PremomApplicationTests.java
+++ src/test/java/com/app/premom/PremomApplicationTests.java
@@ -1,13 +1,12 @@
 package com.app.premom;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PremomApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
